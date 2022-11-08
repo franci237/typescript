357050e91bcd2511ce9b7484062a791df73f7c59
Francesco Colamonici <francesco.colamonici@byebyeq.com>
2022-11-08 10:34:57 +0100

    refactor: move server to its own file

---
+++ src/simple-server.ts
@@ -0,0 +1,46 @@
+import http from 'node:http'
+import url from 'node:url'
+
+// import { doDelete, doGet, doPatch, doPost, doPut } from './user/user.controller'
+import * as userController from './user/user.controller'
+
+const PORT = process.env.PORT || 8080
+
+export function runSimpleServer() {
+	return http.createServer(async (req, res) => {
+		if (!req.url) return res.end()
+
+		const parsedUrl = url.parse(req.url, true)
+		const pathname = parsedUrl.pathname
+		const [, resource] = pathname?.split('/') ?? []
+		const method = req.method?.toUpperCase() ?? ''
+
+		// GET http://localhost:8080/users
+		// GET http://localhost:8080/users/1 [id=1]
+		// POST http://localhost:8080/users { name: 'Paperoga' }
+		if (resource === 'users') {
+			if (method === 'GET') {
+				await userController.doGet(req, res)
+			} else if (method === 'POST') {
+				await userController.doPost(req, res)
+			} else if (method === 'PUT') {
+				// PUT http://localhost:8080/users/:id { name: 'Paperoga' }
+				await userController.doPut(req, res)
+			} else if (method === 'PATCH') {
+				// PATCH http://localhost:8080/users/:id { name: 'Paperoga' }
+				await userController.doPatch(req, res)
+			} else if (method === 'DELETE') {
+				// DELETE http://localhost:8080/users/:id
+				await userController.doDelete(req, res)
+			} else {
+				res.writeHead(405)
+				res.end()
+			}
+		} else {
+			res.writeHead(404)
+			res.end()
+		}
+	})
+		.listen(8080)
+		.on('listening', () => console.log(`Server ready at http://localhost:${PORT}`))
+}
